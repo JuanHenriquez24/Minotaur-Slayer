abad9abf892ba59c4dd449b958e0a50e2b8ba4c6
Sam <89226291+Sam-or-something@users.noreply.github.com>
2022-08-08 11:54:12 -0300

    8/8 (caminar y rotacion en Y)

---
+++ Proyecto/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    [SerializeField] private float sensitivity = 2f;
+    [SerializeField] private Camera cam;
+
+    public void Update()
+    {
+        //rotate on x
+        float mouse_Y = Input.GetAxis("Mouse Y");
+        mouse_Y = Mathf.Clamp(mouse_Y, -80, 80);
+        transform.Rotate(new Vector3(-mouse_Y * sensitivity, 0, 0));
+    }
+}
---
+++ Proyecto/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float walk_speed;
+    [SerializeField] private Camera cam;
+    [SerializeField] private float sensitivity = 4f;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        //camera follow
+        cam.transform.rotation = transform.rotation;
+        cam.transform.position = transform.position;
+
+        //rotate on y
+        float mouse_X = Input.GetAxis("Mouse X");
+        transform.Rotate(0, mouse_X * sensitivity, 0);
+
+        //walk
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, walk_speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -walk_speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(walk_speed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-walk_speed, 0, 0);
+        }
+    }
+}
