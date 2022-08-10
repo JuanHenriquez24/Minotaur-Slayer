583b9ffd0026a878a27be9c77e945a5f806217fc
Sam <89226291+Sam-or-something@users.noreply.github.com>
2022-08-10 14:12:36 -0300

    10/8 (jump, run...)

--- Proyecto/Assets/Scripts/PlayerController.cs
+++ Proyecto/Assets/Scripts/PlayerController.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float walk_speed;
+    [SerializeField] private float run_speed;
+    private float speed;
     [SerializeField] private float sensitivity;
     [SerializeField] private float jump_force;
+    private Rigidbody rb;
     private bool playing;
     public bool on_floor;
-
-    private void Start()
-    {
-    }
+    [SerializeField] private float timer_run;
+    [SerializeField] private float run_cool_down;
+    [SerializeField] private float run_time;
+    [SerializeField] private float run_cool_down_timer;
 
     private void Update()
     {
+        timer_run += Time.deltaTime;
+        run_cool_down_timer += Time.deltaTime;
+        rb = gameObject.GetComponent<Rigidbody>();
         playing = gameObject.GetComponentInParent<Playing>().playing;
 
         if(playing)
@@ -27,33 +33,51 @@ public class PlayerController : MonoBehaviour
             //walk
             if (Input.GetKey(KeyCode.W))
             {
-                transform.Translate(0, 0, walk_speed);
+                transform.Translate(0, 0, speed);
             }
             if (Input.GetKey(KeyCode.S))
             {
-                transform.Translate(0, 0, -walk_speed);
+                transform.Translate(0, 0, speed);
             }
             if (Input.GetKey(KeyCode.D))
             {
-                transform.Translate(walk_speed, 0, 0);
+                transform.Translate(speed, 0, 0);
             }
             if (Input.GetKey(KeyCode.A))
             {
-                transform.Translate(-walk_speed, 0, 0);
+                transform.Translate(-speed, 0, 0);
+            }
+
+            //run
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                timer_run = 0;
+            }
+            else if (Input.GetKeyUp(KeyCode.E))
+            {
+                run_cool_down_timer = 0;
+            }
+            if (Input.GetKey(KeyCode.E) && timer_run < run_time && run_cool_down_timer > run_cool_down)
+            {
+                speed = run_speed;
+            }
+            else
+            {
+                speed = walk_speed;
             }
 
             //jump
             if(on_floor && Input.GetKey(KeyCode.Space))
             {
-                transform.Translate(0, jump_force, 0);
+                rb.AddForce(Vector3.up * jump_force, ForceMode.Impulse);
                 on_floor = false;
             }
         }
     }
 
-    private void OncollisionEnter(Collision col)
+    private void OnTriggerEnter(Collider col)
     {
-        if(col.gameObject.tag == "PISO")
+        if(col.tag == "PISO")
         {
             on_floor = true;
         }
