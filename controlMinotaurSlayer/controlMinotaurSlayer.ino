95af1db2f0bf105e125f637a45550a9019006fb4
SofiaCirelli <101577333+SofiaCirelli@users.noreply.github.com>
2022-08-16 10:26:25 -0300

    Mapeo de joysticks

---
+++ controlMinotaurSlayer/controlMinotaurSlayer.ino
@@ -0,0 +1,44 @@
+#include <Joystick.h>
+
+#define joyX1 A0
+#define joyY1 A1
+#define joySW1 2
+#define joyX2 A2
+#define joyY2 A3
+#define joySW2 3
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(joySW1, INPUT_PULLUP);
+  pinMode(joySW2, INPUT_PULLUP);
+}
+ 
+void loop() {
+  // put your main code here, to run repeatedly:
+  int x1Value = analogRead(joyX1);
+  int y1Value = analogRead(joyY1);
+  int sw1Value = digitalRead(joySW1);
+  int x2Value = analogRead(joyX2);
+  int y2Value = analogRead(joyY2);
+  int sw2Value = digitalRead(joySW2);
+
+  int x1Map = map(x1Value, 0, 1024, -45, 45);
+  int y1Map = map(y1Value, 0, 1024, -45, 45);
+  int x2Map = map(x2Value, 0, 1024, -45, 45);
+  int y2Map = map(y2Value, 0, 1024, -45, 45); 
+ 
+  Serial.flush();  
+  
+  Serial.print(x1Map);
+  Serial.print(",");
+  Serial.print(y1Map);
+  Serial.print(",");
+  Serial.print(sw1Value);
+  Serial.print(x2Map);
+  Serial.print(",");
+  Serial.print(y2Map);
+  Serial.print(",");
+  Serial.println(sw2Value);
+
+  delay(50);
+}
