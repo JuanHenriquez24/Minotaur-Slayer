2782643a45ae2945b7252a78ccb54bcb5e2795ed
SofiaCirelli <101577333+SofiaCirelli@users.noreply.github.com>
2022-08-18 09:12:07 -0300

    mandar datos joystick a unity

---
+++ Proyecto/Assets/Scripts/ControlArduino/SerialConnection.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class SerialConnection : MonoBehaviour
+{
+    
+    SerialPort serialPort = new SerialPort("COM6", 9600);
+
+    void Start()
+    {
+        serialPort.Open();
+
+        serialPort.ReadTimeout = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (serialPort.IsOpen)
+        {
+            try
+            {
+                string lecturaValor = serialPort.ReadLine();
+                Debug.Log(lecturaValor);
+                string[] valor = lecturaValor.Split(',');
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
