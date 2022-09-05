4a0d543bd1ef8cfacc745b7a6d5bad1f98652d36
Sam <89226291+Sam-or-something@users.noreply.github.com>
2022-09-05 11:41:05 -0300

    5/9 (damage cool down, start enemy movement, fix pickup)

---
+++ Proyecto/Assets/Scripts/Movimiento_Enemigo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento_Enemigo : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Transform player;
+    private bool playing;
+    private Transform parent;
+    public Vector3 test;
+
+    void Start()
+    {
+        parent = gameObject.transform.parent;
+        for (int i = 0; i < parent.childCount; i++)
+        {
+            if(parent.GetChild(i).tag == "PLAYER")
+            {
+                player = parent.GetChild(i);
+                break;
+            }
+        }
+    }
+    
+    void Update()
+    {
+        playing = parent.GetComponent<Playing>().playing;
+
+        if (playing)
+        {
+            transform.LookAt(player.transform);
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+    }
+}
