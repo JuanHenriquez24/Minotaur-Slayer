5966f2a89a653f47a6a6ec18a21b6a34fb4f2e3c
Sam <89226291+Sam-or-something@users.noreply.github.com>
2022-09-07 13:59:00 -0300

    7/9 (fix look at enemy, import laberinto)

--- Proyecto/Assets/Scripts/Movimiento_Enemigo.cs
+++ Proyecto/Assets/Scripts/Movimiento_Enemigo.cs
@@ -8,7 +8,7 @@ public class Movimiento_Enemigo : MonoBehaviour
     private Transform player;
     private bool playing;
     private Transform parent;
-    public Vector3 test;
+    private Vector3 look_position;
 
     void Start()
     {
@@ -29,7 +29,8 @@ public class Movimiento_Enemigo : MonoBehaviour
 
         if (playing)
         {
-            transform.LookAt(player.transform);
+            look_position = new Vector3(player.position.x, transform.position.y, player.position.z);
+            transform.LookAt(look_position);
             transform.position += transform.forward * speed * Time.deltaTime;
         }
     }
