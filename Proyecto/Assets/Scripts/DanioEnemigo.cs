1a5cd7ea4bd26c3c17f758179c0905f5b6e56e84
Sam <89226291+Sam-or-something@users.noreply.github.com>
2022-09-19 11:04:33 -0300

    19/9 (ataque)

--- Proyecto/Assets/Scripts/DanioEnemigo.cs
+++ Proyecto/Assets/Scripts/DanioEnemigo.cs
@@ -5,9 +5,36 @@ using UnityEngine;
 public class DanioEnemigo : MonoBehaviour
 {
     [SerializeField] private int vidaMax;
+    public int vidaActual;
+    public int danio;
+    private float damage_cool_down;
+
+    void Start()
+    {
+        damage_cool_down = 5;
+        vidaActual = vidaMax;
+    }
 
     void Update()
     {
+        if(vidaActual <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
 
+    void OnTriggerStay(Collider col)
+    {
+        if (col.tag == "ATTACK")
+        {
+            Debug.Log("GGGGGG");
+            recibirDanio(col.GetComponent<AtaqueJugador>().danio);
+            damage_cool_down = 0;
+        }
+    }
+
+    public void recibirDanio(int danio)
+    {
+        vidaActual -= danio;
     }
 }
