ce99a75035056fc2fdedf7b11d76d27c10754e9a
Sam <89226291+Sam-or-something@users.noreply.github.com>
2022-09-19 09:22:42 -0300

    n

--- Proyecto/Assets/Scripts/AtaqueJugador.cs
+++ Proyecto/Assets/Scripts/AtaqueJugador.cs
@@ -4,40 +4,51 @@ using UnityEngine;
 
 public class AtaqueJugador : MonoBehaviour
 {
-    private bool en_rango_enemigo;
     private bool playing;
     private Collider player_collider;
+    private float attack_timer;
+    private float attack_cool_down_timer;
+    [SerializeField] private float coolDownTime;
+    [SerializeField] private float attackTime;
+    private bool attacking;
+    public bool danioPlayer;
 
     void Start()
     {
         player_collider = gameObject.GetComponent<BoxCollider>();
+        attack_cool_down_timer = coolDownTime;
+
     }
-    
+
     void Update()
     {
         playing = gameObject.transform.parent.GetComponent<Playing>().playing;
         if (playing)
         {
-            if(en_rango_enemigo && Input.GetKeyDown(KeyCode.M))
+            if (attacking)
             {
+                attack_timer += Time.deltaTime;
+            }
+            else
+            {
+                attack_cool_down_timer += Time.deltaTime;
+            }
+
+            if (attack_timer > attackTime)
+            {
+                attacking = false;
+                attack_timer = 0;
+            }
+
+            if (Input.GetKeyDown(KeyCode.M) && attack_cool_down_timer > coolDownTime && !attacking)
+            {
+
+                player_collider.enabled = true;
+                attacking = true;
+                attack_cool_down_timer = 0;
 
             }
         }
     }
-
-    private void OnTriggerEnter(Collider col)
-    {
-        if(col.tag == "PAIN")
-        {
-            en_rango_enemigo = true;
-        }
-    }
-    
-    private void OnTriggerExit (Collider col)
-    {
-        if(col.tag == "PAIN")
-        {
-            en_rango_enemigo = false;
-        }
-    }
 }
+    
