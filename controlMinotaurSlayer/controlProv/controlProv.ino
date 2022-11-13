cf746c77565e131b87079321f75ee44e1af1bcd4
Sofía <101577333+SofiaCirelli@users.noreply.github.com>
2022-11-12 23:55:39 -0300

    Update controlProv.ino

--- controlMinotaurSlayer/controlProv/controlProv.ino
+++ controlMinotaurSlayer/controlProv/controlProv.ino
@@ -11,9 +11,16 @@
 #define bot2 5
 #define bot3 6
 #define bot4 7
+#define vib 8
+
+int range = 12;             // output range of X or Y movement
+int responseDelay = 5;      // response delay of the mouse, in ms
+int threshold = range / 4;  // resting 0
+int center = range / 2;     // resting position value
+bool mouseIsMoving = true;
 
 void setup() {
-  Serial.begin(115000);
+  Serial.begin(115200);
   Keyboard.begin();
   Mouse.begin();
 
@@ -23,133 +30,102 @@ void setup() {
   pinMode(bot2, INPUT_PULLUP);
   pinMode(bot3, INPUT_PULLUP);
   pinMode(bot4, INPUT_PULLUP);
+  pinMode(vib, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int x1Value = analogRead(joyX1);
-  int y1Value = analogRead(joyY1);
   int sw1Value = !digitalRead(joySW1);
-  int x2Value = analogRead(joyX2);
-  int y2Value = analogRead(joyY2);
   int sw2Value = !digitalRead(joySW2);
   int bot1Value = !digitalRead(bot1);
   int bot2Value = !digitalRead(bot2);
   int bot3Value = !digitalRead(bot3);
   int bot4Value = !digitalRead(bot4);
+  int vibra = Serial.read();
+  
+  digitalWrite(vib, LOW);
 
-  int threshold = 10;
-  int x1Map = map(x1Value, 0, 1024, -46, 45);
-  int y1Map = map(y1Value, 0, 1024, 45, -46);
-  int x2Map = map(x2Value, 0, 1024, -46, 45);
-  int y2Map = map(y2Value, 0, 1024, 45, -46);
+  int x1Map = readAxis(joyX1);
+  int y1Map = readAxis(joyY1);
+  int x2Map = readAxis(joyX2);
+  int y2Map = readAxis(joyY2);
+
 
   // use the pushbuttons to control the keyboard:
-  if (x1Map > threshold) {
-    // adelante
-    Keyboard.press('w');
-    delay(50);
-
-  } else {
-    Keyboard.release('w');
+  if (x1Map < 0) {
+    Keyboard.write('a');   // izquierda
+  }
+  
+  if (x1Map > 0) {
+    Keyboard.write('d'); // derecha
   }
 
-  if (x1Map < !threshold) {
-    // atras
-    Keyboard.press('s');
-    delay(50);
-
-  } else {
-    Keyboard.release('s');
+  if (y1Map > 0) {
+    Keyboard.write('s'); // abajo
   }
 
-  if (y1Map > threshold) {
-    // derecha
-    Keyboard.press('d');
-    delay(50);
-
-  } else {
-    Keyboard.release('d');
+  if (y1Map < 0) {
+    Keyboard.write('w'); // arriba
   }
 
-  if (y1Map < !threshold) {
-    // izquierda
-    Keyboard.press('a');
-    delay(50);
-
-  } else {
-    Keyboard.release('a');
-  }
 
   if (sw1Value == HIGH) {
-    // pausar
-    Keyboard.press('p');
-    delay(50);
-
-  } else {
-    Keyboard.release('p');
+    Keyboard.write('p');    // pausar
   }
 
   if (sw2Value == HIGH) {
-    // correr
-    Keyboard.press('e');
-    delay(50);
-
-  } else {
-    Keyboard.release('e');
+    Keyboard.write('e');    // correr
   }
 
+   
   if (bot1Value == HIGH) {
-    // atacar / agarrar objetos
-    Keyboard.press('m');
-    delay(50);
-
-  } else {
-    Keyboard.release('m');
+    Keyboard.write('m');  // atacar / agarrar objetos
   }
+
+
   if (bot2Value == HIGH) {
-    // saltar
-    Keyboard.press(32);
-    delay(50);
-
-  } else {
-    Keyboard.release(32);
+    Keyboard.write(32); // saltar
   }
+
+ 
   if (bot3Value == HIGH) {
+    Keyboard.write('q');
     // tirar hilo
-    Keyboard.press('q');
-    delay(50);
-
-  } else {
-    Keyboard.release('q');
   }
+
   if (bot4Value == HIGH) {
-    // inventario
-    Keyboard.press('f');
-    delay(50);
-
-  } else {
-    Keyboard.release('f');
+    Keyboard.write('f');
   }
 
-  if (x2Map > threshold) {
-    // arriba
-    Mouse.move(10, 0);
+
+  if(mouseIsMoving){
+    Mouse.move(x2Map, y2Map, 0);
   }
-  if (x2Map < !threshold) {
-    // abajo
-    Mouse.move(-10, 0);
+
+  if(vibra == 1){
+    digitalWrite(vib, HIGH);
+    digitalWrite(LED_BUILTIN, HIGH);
   }
-  if (y2Map > !threshold) {
-    // derecha
-    Mouse.move(0, -10);
+  else{
+    digitalWrite(vib, LOW);
+    digitalWrite(LED_BUILTIN, LOW);
   }
-  if (y2Map < threshold) {
-    // izquierda
-    Mouse.move(0, 10);
+
+  delay(responseDelay);
+} 
+
+int readAxis(int thisAxis) {
+  
+  int reading = analogRead(thisAxis);
+
+  reading = map(reading, 0, 1023, 0, range);
+
+  int distance = reading - center;
+
+  if (abs(distance) < threshold) {
+    distance = 0;
   }
-  if (bot4Value == HIGH && bot3Value == HIGH) {
-    Keyboard.end();
-    Mouse.end();
-  }
+
+  // return the distance for this axis:
+  return distance;
 }
